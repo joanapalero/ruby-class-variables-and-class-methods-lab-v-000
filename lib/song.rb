01ab1234d1a3715e3e24e6d8d09b6f89080e7778
Joana Palero <joana.palero24@gmail.com>
2018-06-28 21:54:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,10 @@
 class Song
+  
   @@count = 0
   @@genres = []
   @@artists = []
   @@genre_count = {}
   @@artists_count = {}
-  
-  
-  
   
   def initialize(name, artist, genre)
     @name = name
